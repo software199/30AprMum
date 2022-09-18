156211160d8e4003b258241eb8b809364b4aade5
dell <dell@DESKTOP-D5TKSH9>
2022-09-18 09:57:21 +0530

    [Rutuja] created demo1,demo2,demo3 file

---
+++ src/sample/demo01.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class demo01
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("good morning");
+		
+		
+		
+	}
+
+}
---
+++ src/sample/demo03.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class demo03 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hello");
+		
+	}
+
+}
